8cf9d0504c3cb06a0820fc2d25e7ce89632f9aa7
Priyashree-T <priyashreethyagarajan.88@gmail.com>
2026-02-26 13:07:11 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -1,6 +1,18 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class EvenOdd {
     public static void main(String[] args) {
-        // This line prints "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+
+        scanner.close();
     }
 }
